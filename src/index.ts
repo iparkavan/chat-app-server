124b39ad66363d08361060dc66c2297dd3cf08b9
Park Avan <ipark.avan@gmail.com>
2025-03-07 15:35:40 +0530

    some changes

--- src/index.ts
+++ src/index.ts
@@ -33,8 +33,8 @@ const nextURL = process.env.ORIGIN as string;
 // );
 
 const allowedOrigins = [
-  "http://localhost:3000",
   "https://chat-app-client-rose.vercel.app",
+  "http://localhost:3000",
 ];
 
 app.use(
